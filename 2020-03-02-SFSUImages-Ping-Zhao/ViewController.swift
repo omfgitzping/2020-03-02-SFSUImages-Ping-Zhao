63a598917989df740b34c0e957fdb11807a27313
Ping Zhao <pzhao11@gmail.com>
2020-03-02 20:42:07 -0800

    3/2 Class Project

--- 2020-03-02-SFSUImages-Ping-Zhao/ViewController.swift
+++ 2020-03-02-SFSUImages-Ping-Zhao/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabelSfsu: UILabel!
+    
+    @IBOutlet weak var imageViewSfsu: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        messageLabelSfsu.text = ""
     }
 
-
+    @IBAction func messageButtonPresssed(_ sender: UIButton) {
+        messageLabelSfsu.text = "Spider-Man: Into the Spider-Verse"
+        imageViewSfsu.image = UIImage(named: "image1")
+   
+    }
+    
+    @IBAction func messageButton2Pressed(_ sender: UIButton) {
+        messageLabelSfsu.text = "San Francisco State University"
+        imageViewSfsu.image = UIImage(named: "image0")
+    }
+    
 }
 
